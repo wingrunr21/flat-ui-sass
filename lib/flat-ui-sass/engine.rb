a76ffc632165135d4e0198aaf25e59eab0615f4f
Stafford Brunk <wingrunr21@gmail.com>
2014-01-23 09:42:12 -0700

    Add Flat-UI-Icons to the asset precompile list

--- lib/flat-ui-sass/engine.rb
+++ lib/flat-ui-sass/engine.rb
@@ -1,9 +1,13 @@
 module FlatUI
   module Rails
     class Engine < ::Rails::Engine
-      #initializer "flat-ui-sass.assets.precompile" do |app|
-        #app.config.assets.precompile << %r(bootstrap/glyphicons-halflings-regular\.(?:eot|svg|ttf|woff)$)
-      #end
+      initializer "flat-ui-sass.assets.precompile" do |app|
+        if Dir.exist? File.join(::Rails.root, 'vendor', 'assets', 'fonts', 'flat-ui-pro')
+          app.config.assets.precompile << %r(flat-ui-pro/Flat-UI-Icons\.(?:eot|svg|ttf|woff)$)
+        else
+          app.config.assets.precompile << %r(flat-ui/Flat-UI-Icons\.(?:eot|svg|ttf|woff)$)
+        end
+      end
     end
 
     class Railtie < ::Rails::Railtie
