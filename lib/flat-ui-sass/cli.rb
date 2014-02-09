4a828be809810b571c8ba0cc985c9c4fcfb6dc57
Stafford Brunk <wingrunr21@gmail.com>
2014-02-08 20:26:21 -0700

    Implement the fui_convert executable

---
+++ lib/flat-ui-sass/cli.rb
@@ -0,0 +1,52 @@
+require 'optparse'
+require 'tasks/converter'
+require 'flat-ui-sass/version'
+
+module FlatUI
+  class CLI
+    class << self
+      def start(*args)
+        options = parse_options!(*args)
+        Converter.new(options[:type], options[:input]).process_flat_ui!
+      end
+
+      private
+
+      def parse_options!(*args)
+        options = {
+          type: :pro,
+          input: 'flat-ui-pro'
+        }
+
+        opt_parser = OptionParser.new do |opts|
+          opts.banner = "Usage: fui_convert [options]"
+          opts.separator ""
+          opts.separator "Options:"
+
+          opts.on("--type [TYPE]", "-t", [:free, :pro], "Specify the type of conversion to perform (free or pro).","Default is pro") do |type|
+            if type
+              options[:type] = type.to_sym
+              options[:input] = "flat-ui" if type == :free
+            end
+          end
+          opts.on("--input [DIR]", "-i", "The Flat-UI root directory.","Default is flat-ui-pro") do |dir|
+            options[:input] = dir if dir
+          end
+          opts.on_tail("-h", "--help", "Show help") do
+            puts opts
+            exit
+          end
+          opts.on_tail("--version", "Show version") do
+            puts "Flat-UI Compatibility:"
+            puts "  Free v#{FlatUI::VERSION}"
+            puts "  Pro v#{FlatUI::PRO_VERSION}"
+            exit
+          end
+        end
+
+        opt_parser.parse!(args)
+        options
+      end
+    end
+  end
+end
