38c25f95a885bcfb31fb2a11e915e4f2a0d6d112
Stafford Brunk <wingrunr21@gmail.com>
2015-03-20 10:35:00 -0600

    Fix font asset precompile

--- lib/flat-ui-sass/engine.rb
+++ lib/flat-ui-sass/engine.rb
@@ -3,9 +3,9 @@ module FlatUI
     class Engine < ::Rails::Engine
       initializer "flat-ui-sass.assets.precompile" do |app|
         if Dir.exist? File.join(::Rails.root, 'vendor', 'assets', 'fonts', 'flat-ui-pro')
-          app.config.assets.precompile << %r(flat-ui-pro/flat-ui-icons-regular\.(?:eot|svg|ttf|woff)$)
+          app.config.assets.precompile << %r(flat-ui-pro/glyphicons/flat-ui-pro-icons-regular\.(?:eot|svg|ttf|woff)$)
         else
-          app.config.assets.precompile << %r(flat-ui/flat-ui-icons-regular\.(?:eot|svg|ttf|woff)$)
+          app.config.assets.precompile << %r(flat-ui/glyphicons/flat-ui-icons-regular\.(?:eot|svg|ttf|woff)$)
         end
       end
     end
